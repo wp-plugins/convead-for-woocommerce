4391c25c38cfe9c65f21beec056a6bac377a0b68
convead <convead@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-10 14:19:38 +0000

    Release 1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/convead-for-woocommerce/tags/1.0.4@1242510 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ convead.php
@@ -0,0 +1,31 @@
+<?php
+/*
+Plugin Name: Convead
+Description:
+Version: 1.0.4
+Author: Joomline
+Author URI: http://joomline.ru
+*/
+
+/*  Copyright 2015  Joomline  (email: sale@joomline.ru)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+define( 'CONVEAD_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'CONVEAD_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+require_once (CONVEAD_PLUGIN_DIR . 'includes/convead.class.php');
+
+//Инициализация
+add_action( 'init', array( 'Convead', 'init' ) );
